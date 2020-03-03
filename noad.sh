651e270b4427e3d620754c5aca1163a681a14e46
root <root@2018sh001-0102767.novalocal>
2020-03-03 13:30:56 +0800

    web

---
+++ noad.sh
@@ -0,0 +1,6 @@
+wget -P /opt/web/noad/old -N -c -t 0 https://easylist-downloads.adblockplus.org/easylist.txt
+wget -P /opt/web/noad/old -N -c -t 0 https://easylist-downloads.adblockplus.org/easyprivacy.txt
+wget -P /opt/web/noad/old -N -c -t 0 https://easylist-downloads.adblockplus.org/easylistchina.txt
+wget -P /opt/web/noad/old -N -c -t 0 https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt
+wget -P /opt/web/noad/old -N -c -t 0 http://git.oschina.net/halflife/list/raw/master/ad.txt
+wget -P /opt/web/noad/old -N -c -t 0 https://gitee.com/banbendalao/adguard/raw/master/ADgk.txt
